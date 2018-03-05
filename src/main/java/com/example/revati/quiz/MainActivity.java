c4d51ef19bae0d31cbbbde0d74cc406a1d38225f
Revati Singh <revatisngh588@gmail.com>
2018-03-05 23:39:16 +0530

    Adding functionality to start button
    Starting another activity for question one of quiz

--- src/main/java/com/example/revati/quiz/MainActivity.java
+++ src/main/java/com/example/revati/quiz/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.revati.quiz;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,question_one.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
