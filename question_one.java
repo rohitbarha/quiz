d2a6a54dea321937c0e387967960f17015bffe2f
Revati Singh <revatisngh588@gmail.com>
2018-03-07 00:26:33 +0530

    Adding functionality to radioButton choices

--- question_one.java
+++ question_one.java
@@ -3,7 +3,10 @@ package com.example.revati.quiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class question_one extends AppCompatActivity {
 
@@ -15,5 +18,15 @@ public class question_one extends AppCompatActivity {
         TextView questionTextView = (TextView)findViewById(R.id.question);
         questionTextView.setText(Html.fromHtml("<b>"+"Question 1:\n".toUpperCase()+"</b>"+"<br>"+"<br>"+"Cocoa : Chocolate : : _________ : _________"+"</br>"+"</br>"));
 
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radio4)
+                    Toast.makeText(question_one.this,"Correct Choice",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(question_one.this,"Incorrect Choice",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
